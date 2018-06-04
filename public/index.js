e16a653ff9597d0eeb5a3016df92c58fdcf26b0f
nosnatef <gushua1984@gmail.com>
2018-06-04 02:04:18 -0700

    basic API implement

--- public/index.js
+++ public/index.js
@@ -1,1 +1,48 @@
 $('.dropdown-toggle').dropdown();
+
+function uploadForm(){
+  var username = submitForm['name'].value;
+  var platform;
+  if(submitForm['platform-psn'].checked){
+    platform = submitForm['platform-psn'].value;
+  }
+  else if(submitForm['platform-xbl'].checked){
+    platform = submitForm['platform-xbl'].value;
+  }
+  else{
+    platform = submitForm['platform-steam'].value;
+  }
+
+  var request = new XMLHttpRequest();
+  var requestURL = '/callofduty/wwii/submit';
+  request.open('POST',requestURL);
+  var userObj = {
+    username:username,
+    platform:platform,
+    title:'wwii',
+    days:1,
+    type:'core',
+    time:'monthly',
+    mode:'career'
+  };
+  console.log("this is",userObj);
+  var requestBody = JSON.stringify(userObj);
+  request.setRequestHeader(
+    'Content-Type','application/json'
+  );
+  request.addEventListener('load',function(event){
+    if(event.target.status!=200){
+      alert("form submit failure");
+    }
+    else{
+
+    }
+  });
+
+  request.send(requestBody);
+}
+
+var submitButton = document.getElementById('submit-button');
+var submitForm = document.forms[0];
+console.log(submitForm);
+submitButton.addEventListener('click',uploadForm);
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const overwatch = require('overwatch-api');
 const steamAPI = require('steam-api');
+const codAPI = require('cod-api');
+const bodyParser = require('body-parser');
+
 var path = require('path');
 var fs = require('fs');
 var exphbs = require('express-handlebars');
@@ -15,6 +18,7 @@ const tag = 'JayBreak-1527';
 
 const steamAPIKey = '36E4FE8D8ABDAB3E874F2111676BFFAF';
 
+var appID = 311210;
 /*
 overwatch.getProfile(platform,region,tag,function(json){
   console.log(json);
@@ -22,26 +26,63 @@ overwatch.getProfile(platform,region,tag,function(json){
 */
 
 var user = new steamAPI.User(steamAPIKey,76561198272110510);
+var userStats = new steamAPI.UserStats(steamAPIKey);
 
+/*
 user.GetPlayerBans().done(function(result){
   console.log(result);
 });
 
+userStats.GetNumberOfCurrentPlayers(appID).done(function(result){
+  console.log(result);
+});
+
+userStats.GetSchemaForGame(appID).done(function(result){
+  console.log(result);
+});
+
+
 app.get('/123',function(req,res){
   user.GetPlayerBans().done(function(result){
     console.log(result);
-    
   });
 });
+*/
 
 /*user.GetFriendList().done(function(result){
   console.log(result);
 });
 */
 
+var options = {
+  title:"iw",
+  platform:"steam",
+  username:"Noob Smg",
+  days:1,
+  type:"core",
+  time:"monthly",
+  mode:"career"
+};
+
+app.use(bodyParser.json());
+
+codAPI.getProfile(options,function(profile){
+  console.log(profile);
+});
+
 
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
 });
+
+app.post('/callofduty/wwii/submit',function(req,res){
+  console.log(req.body);
+  console.log("   - username:",req.body.username);
+  console.log("   - platform:",req.body.platform);
+  codAPI.getProfile(req.body,function(profile){
+    console.log(profile);
+  });
+});
+
 
 app.use(express.static('public'));
